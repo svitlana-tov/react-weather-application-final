c6d3d80261d8536f4a124cff1d46b681263d4d47
svitlana-tov <147521374+svitlana-tov@users.noreply.github.com>
2024-01-14 10:57:57 -0500

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <h1>
+          {" "}
+          <Weather />
+        </h1>
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/svitlana-tov"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Svitlana Tovkaniuk
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/svitlana-tov/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-8">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-4">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+              className="d-flex"
+            />
+            <div className="d-flex">
+              <span className="temperature">2</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:15%</li>
+            <li>Humidity: 75%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
